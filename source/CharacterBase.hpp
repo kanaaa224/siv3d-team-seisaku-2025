e8da2d80db0d77471f597c07951c098fae743e98
kanaaa224 <kanaaa224@outlook.com>
2025-05-12 18:03:30 +0900

    [ new ] オブジェクトベース、キャラクターベース、プレイヤークラス 作成

---
+++ source/CharacterBase.hpp
@@ -0,0 +1,14 @@
+﻿# pragma once
+
+# include "ObjectBase.hpp"
+
+class CharacterBase : public ObjectBase {
+protected:
+	float hp, max_hp;
+
+public:
+	CharacterBase(const Vec2& start_position) : ObjectBase(start_position) {}
+
+	float getHP()    const { return hp; }
+	float getMaxHP() const { return max_hp; }
+};
