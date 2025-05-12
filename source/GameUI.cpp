bf08cfcc24877673370dd07690c25781f6fca0f2
shiona0820 <138412503+shiona0820@users.noreply.github.com>
2025-05-12 14:37:58 +0900

    GameUIのファイル

---
+++ source/GameUI.cpp
@@ -0,0 +1,34 @@
+﻿#include "GameUI.h"
+
+// 静的メンバ変数定義 nullptrで初期化
+GameUI* GameUI::instance = nullptr;
+
+void GameUI::Initialize()
+{
+	//
+}
+
+void GameUI::Update()
+{
+	//
+}
+
+void GameUI::Draw()
+{ 
+	//
+}
+
+void GameUI::Finalize()
+{
+	//
+}
+
+GameUI* GameUI::GetInstance()
+{
+	if (instance == nullptr)
+	{
+		instance = new GameUI();
+	}
+
+	return instance;
+}
