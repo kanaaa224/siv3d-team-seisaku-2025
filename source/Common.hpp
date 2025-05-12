ed0738c64d7550f2b8975aeb468a8066aa3ab8da
kanaaa224 <kanaaa224@outlook.com>
2025-05-12 18:19:06 +0900

    [ new ] 各シーンクラス 作成

---
+++ source/Common.hpp
@@ -0,0 +1,22 @@
+﻿# pragma once
+# include <Siv3D.hpp> // Siv3D v0.6.16
+
+// シーンのステート
+enum class State
+{
+	Title,
+	Game,
+	Ranking,
+};
+
+// 共有するデータ
+struct GameData
+{
+	// 直前のゲームのスコア
+	int32 lastScore = 0;
+
+	// ハイスコア
+	Array<int32> highScores = { 10, 8, 6, 4, 2 };
+};
+
+using App = SceneManager<State, GameData>;
