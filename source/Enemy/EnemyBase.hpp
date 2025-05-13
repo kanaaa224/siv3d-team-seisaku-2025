2309a0d3c70065be9e7cb96e40469162bb2f012c
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-05-13 11:19:09 +0900

    [add] EnemyBaseClassの作成

---
+++ source/Enemy/EnemyBase.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+//親クラス
+#include "../CharacterBase.hpp"
+
+class EnemyBase : public CharacterBase
+{
+public:
+	EnemyBase(const Vec2& start_position) : CharacterBase(start_position) {};
+	~EnemyBase();
+
+	virtual void update() override;
+};
+
