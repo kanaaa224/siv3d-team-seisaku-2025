9b940dbdd998896973645b3f35a4013cd162d834
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-05-16 11:50:17 +0900

    [ new ] カメラ クラス

---
+++ source/Objects/Camera.cpp
@@ -0,0 +1,31 @@
+﻿# include "Camera.hpp"
+
+Camera::Camera(const Vec2& start_position): ObjectBase(start_position)
+{
+	this->initialize();
+}
+
+Camera::~Camera()
+{
+	this->finalize();
+}
+
+void Camera::initialize()
+{
+
+}
+
+void Camera::update()
+{
+
+}
+
+void Camera::draw() const
+{
+	TextureAsset(U"Stage 1 Background").resized(Scene::Size()).draw(position); // 仮
+}
+
+void Camera::finalize()
+{
+
+}
