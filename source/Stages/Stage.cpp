8a876b87aeaf8ebda4de97263504496d3dcb4dc5
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-05-16 12:51:55 +0900

    [ update ] ステージクラス

---
+++ source/Stages/Stage.cpp
@@ -0,0 +1,16 @@
+﻿# include "Stage.hpp"
+
+Stage* Stage::instance = nullptr;
+
+Stage::Stage() {}
+
+void Stage::update() {}
+
+void Stage::draw() const {}
+
+Stage* Stage::GetInstance()
+{
+	if (instance == nullptr) instance = new Stage();
+
+	return instance;
+}
---
+++ source/Stages/Stage1.hpp
@@ -0,0 +1,21 @@
+﻿# pragma once
+
+# include "stage.hpp"
+
+class Stage1 : public Stage
+{
+public:
+	Stage1();
+	~Stage1();
+
+	void update() override;
+	void draw() const override;
+
+	static Stage1* GetInstance();
+
+private:
+	static Stage1* instance;
+
+	void initialize();
+	void finalize();
+};
