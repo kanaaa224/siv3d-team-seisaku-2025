25eb5e32632d9c25355e1bfd0a127a8f755a1923
kanaaa224 <kanaaa224@outlook.com>
2025-05-16 13:46:40 +0900

    [ update ] カスタムイメージローダー 修正

--- source/Utils/CustomImageLoader.hpp
+++ source/Utils/CustomImageLoader.hpp
@@ -3,21 +3,4 @@
 # include <Siv3D.hpp>
 
 // 分割画像登録: path(画像のファイルパス) size(分割したい画像のサイズ)
-Array<TextureRegion> LoadDivGraph(const AssetNameView& name, const Size& image_size)
-{
-	TextureAsset texture = TextureAsset(name);
-
-	if (image_size.x <= 0 || image_size.y <= 0) { throw Error{ U"切り出しサイズが不正です: {}"_fmt(image_size) }; }
-
-	// 警告：端が余るかも
-	if ((texture.width() % image_size.x != 0) || (texture.height() % image_size.y != 0)) { Print << U"警告: 切り出しサイズが画像とぴったり一致していません"; }
-
-	Array<TextureRegion> textureRegions;
-
-	for (auto p : step(Size(texture.width() / image_size.x, texture.height() / image_size.y)))
-	{
-		textureRegions.push_back(texture(p * image_size, image_size));
-	}
-
-	return textureRegions;
-}
+Array<TextureRegion> LoadDivGraph(const AssetNameView& name, const Size& image_size);
