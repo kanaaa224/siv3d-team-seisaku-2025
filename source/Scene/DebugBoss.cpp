d42f78452b6a5b1edd9c0c34e36cc68962635107
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-05-16 10:28:11 +0900

    [ new ] 島袋が使うデバッグ用シーン

---
+++ source/Scene/DebugBoss.cpp
@@ -0,0 +1,17 @@
+﻿# include "DebugBoss.hpp"
+
+DebugBoss::DebugBoss(const InitData& init)
+	: IScene{ init }
+{
+
+}
+
+void DebugBoss::update()
+{
+	if (Key1.down() || MouseL.down()) changeScene(SceneState::Title, 0.5s);
+}
+
+void DebugBoss::draw() const
+{
+
+}
