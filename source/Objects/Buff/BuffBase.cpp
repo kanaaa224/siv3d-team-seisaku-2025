8bc7ef5217faedd925fca48c64cca776fdaceb34
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-05-19 11:31:55 +0900

    [add] BuffClassの作成開始

---
+++ source/Objects/Buff/BuffBase.cpp
@@ -0,0 +1,18 @@
+﻿#include "BuffBase.hpp"
+
+BuffBase::BuffBase(const Vec2& start_position) :
+	ObjectBase(start_position)//リスポーン位置
+{
+}
+
+BuffBase::~BuffBase()
+{
+}
+
+void BuffBase::update()
+{
+}
+
+void BuffBase::draw() const
+{
+}
---
+++ source/Objects/Buff/BuffBase.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "../ObjectBase.hpp"
+
+class BuffBase : public ObjectBase
+{
+public:
+	BuffBase(const Vec2& start_position);
+	virtual ~BuffBase();
+
+	virtual void update() override;
+	virtual void draw() const override;
+};
+
