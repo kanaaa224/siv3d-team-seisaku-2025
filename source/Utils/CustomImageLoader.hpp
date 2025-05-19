4dba8b2371d4fb1a079a04b9c0648bee40f18311
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-05-19 11:54:02 +0900

    [ optimize ] カスタムイメージローダー 最適化

--- source/Utils/CustomImageLoader.hpp
+++ source/Utils/CustomImageLoader.hpp
@@ -2,5 +2,5 @@
 
 # include <Siv3D.hpp>
 
-// 分割画像登録: path(画像のファイルパス) size(分割したい画像のサイズ)
+// 分割画像登録: name(画像のアセット名), size(分割したい画像のサイズ)
 Array<TextureRegion> LoadDivGraph(const AssetNameView& name, const Size& image_size);
