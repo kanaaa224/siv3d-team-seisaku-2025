c3e35e64e0acbc1dd4981ec157af21565255e45c
kanaaa224 <kanaaa224@outlook.com>
2025-05-19 11:54:02 +0900

    [ optimize ] カスタムイメージローダー 最適化

--- source/Utils/CustomImageLoader.cpp
+++ source/Utils/CustomImageLoader.cpp
@@ -4,10 +4,9 @@ Array<TextureRegion> LoadDivGraph(const AssetNameView& name, const Size& image_s
 {
 	TextureAsset texture = TextureAsset(name);
 
-	if (image_size.x <= 0 || image_size.y <= 0) { throw Error{ U"切り出しサイズが不正です: {}"_fmt(image_size) }; }
+	if (image_size.x <= 0 || image_size.y <= 0) throw Error{ U"切り出しサイズが不正です: {}"_fmt(image_size) };
 
-	// 警告：端が余るかも
-	if ((texture.width() % image_size.x != 0) || (texture.height() % image_size.y != 0)) { Print << U"警告: 切り出しサイズが画像とぴったり一致していません"; }
+	if ((texture.width() % image_size.x != 0) || (texture.height() % image_size.y != 0)) Print << U"警告: 切り出しサイズが画像と一致していません";
 
 	Array<TextureRegion> textureRegions;
 
