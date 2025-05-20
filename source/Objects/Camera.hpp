c31e6264a80fd987035d3a30e2b8daefcf11a197
moyakino <104491816+moyakino@users.noreply.github.com>
2025-05-20 11:18:02 +0900

    Camera.cpp 関数追加

--- source/Objects/Camera.hpp
+++ source/Objects/Camera.hpp
@@ -4,12 +4,20 @@
 
 class Camera : public ObjectBase
 {
+private:
+	Camera2D camera;					//カメラ
+	double cameraPosX;					//カメラのX座標
+	double cameraCenterOffset;			//カメラの中心
+	double cameraCenterOffsetVelocity;	//カメラの方向
+
 public:
 	Camera(P2World& world, const Vec2& position);
 	~Camera();
 
 	void update() override;
 	void draw() const override;
+
+	void SetCameraPosX(Vec2 pos);
 
 private:
 	void initialize();
