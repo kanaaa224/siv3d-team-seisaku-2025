88ef1a6518829a2077481fc0e8e1571c9f3b8bc7
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-05-21 13:03:47 +0900

    [other] コードの整理

--- source/Objects/Buff/BuffBase.cpp
+++ source/Objects/Buff/BuffBase.cpp
@@ -12,20 +12,9 @@ BuffBase::~BuffBase()
 
 void BuffBase::update()
 {
-	//重力
-	//velocity.y += GRAVITY;
-
-	//バフの動き
-	//drop_movement();
-
-	//座標位置の更新
-	//position += velocity * Scene::DeltaTime();
 
 #ifdef DEBUG
-	//仮地面
-	/*if (position.y >= 400.0) {
-		velocity.y = 0.0;
-	}*/
+
 #endif // DEBUG
 
 }
