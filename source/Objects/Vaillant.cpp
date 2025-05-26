18ee9dcb6addffed46f78117c73ea8d3e119136b
kanaaa224 <kanaaa224@outlook.com>
2025-05-26 12:06:29 +0900

    [ new ] ボス1 実装開始

---
+++ source/Objects/Vaillant.cpp
@@ -0,0 +1,68 @@
+﻿# include "Vaillant.hpp"
+# include "../Stages/Stage.hpp"
+# include "./Player.hpp"
+
+Vaillant::Vaillant(P2World& world, const Vec2& position) : CharacterBase(world, position), start_position(position)
+{
+	body = world.createRect(P2Dynamic, position, SizeF{ 75, 100 });
+
+	body.setFixedRotation(true);
+
+	hp = 100;
+
+	this->initialize();
+}
+
+Vaillant::~Vaillant()
+{
+	this->finalize();
+}
+
+void Vaillant::initialize()
+{
+
+}
+
+void Vaillant::update()
+{
+	if (body.getPos().y >= 1000 || hp <= 0)
+	{
+		Stage::GetInstance()->deleteObject(this);
+	}
+}
+
+void Vaillant::draw() const
+{
+	body.drawFrame();
+}
+
+void Vaillant::onHit(ObjectBase& object)
+{
+	if (Player* player = dynamic_cast<Player*>(&object))
+	{
+		if (object.getBody().getPos().y < (body.getPos().y - 100))
+		{
+			object.getBody().applyLinearImpulse(Vec2{ 0, -300 });
+
+			addHP(-10);
+		}
+		else
+		{
+			if (object.getBody().getPos().x < body.getPos().x)
+			{
+				object.getBody().applyLinearImpulse(Vec2{ -100, -100 });
+			}
+			else
+			{
+				object.getBody().applyLinearImpulse(Vec2{ 100, -100 });
+			}
+
+			player->addHP(-10);
+		}
+	}
+}
+
+void Vaillant::finalize()
+{
+
+}
---
+++ source/Objects/Vaillant.hpp
@@ -0,0 +1,21 @@
+﻿# pragma once
+
+# include "CharacterBase.hpp"
+
+class Vaillant : public CharacterBase
+{
+public:
+	Vaillant(P2World& world, const Vec2& position);
+	~Vaillant();
+
+	void update() override;
+	void draw() const override;
+
+	void onHit(ObjectBase& object) override;
+
+private:
+	Vec2 start_position;
+
+	void initialize();
+	void finalize();
+};
