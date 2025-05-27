331b9b9a23c0125bc01e84d258fabf67a566bc72
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-05-27 11:54:16 +0900

    [fix] デバッグのコメントの修正

--- source/Objects/Enemy/Scarerun/Scarerun.cpp
+++ source/Objects/Enemy/Scarerun/Scarerun.cpp
@@ -56,14 +56,14 @@ void Scarerun::draw() const
 	now_texture.mirrored(img_flipFlg).resized(size).drawAt(body.getPos());
 	body.drawFrame();
 	
-	Print << U"Enemy_Velocity : " << body.getVelocity();
-	Print << U"Enemy_SpawnPos : " << spawnPosition;
-	Print << U"Enemy_Pos : " << body.getPos();
-	Print << U"NowState : " << nowState;
-	Print << U"OldState : " << oldState;
-	Print << U"StateNum : " << nowStateTime;
-	Print << U"HP : " << hp;
-	Print << U"FlipFlg : " << img_flipFlg;
+	Print << U"Enemy_Scarerun_Velocity : " << body.getVelocity();
+	Print << U"Enemy_Scarerun_SpawnPos : " << spawnPosition;
+	Print << U"Enemy_Scarerun_Pos : " << body.getPos();
+	Print << U"Enemy_Scarerun_NowState : " << nowState;
+	Print << U"Enemy_Scarerun_OldState : " << oldState;
+	Print << U"Enemy_Scarerun_StateNum : " << nowStateTime;
+	Print << U"Enemy_Scarerun_HP : " << hp;
+	Print << U"Enemy_Scarerun_FlipFlg : " << img_flipFlg;
 }
 
 void Scarerun::stateControl()
