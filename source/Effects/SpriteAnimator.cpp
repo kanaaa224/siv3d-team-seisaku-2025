3622e147203af0843fd6cfb0559b3f829c1b89f9
kanaaa224 <kanaaa224@outlook.com>
2025-06-04 13:07:05 +0900

    [ new ] 画像アセットのエフェクター 実装中

---
+++ source/Effects/SpriteAnimator.cpp
@@ -0,0 +1,22 @@
+﻿# include "SpriteAnimator.hpp"
+
+SpriteAnimator::SpriteAnimator(AnimationName animationName = AnimationName::None, bool looping = false) : frameTime(0.0), animationName(animationName), looping(looping) {}
+
+void SpriteAnimator::update()
+{
+	frameTime += Scene::DeltaTime();
+}
+
+void SpriteAnimator::draw() const
+{
+	switch (animationName)
+	{
+	case AnimationName::None:
+	default:
+		break;
+	}
+
+#ifdef _DEBUG
+
+#endif
+}
