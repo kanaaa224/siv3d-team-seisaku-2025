be80e91a1902406a5f42a19edd8e8ff433be25e6
kanaaa224 <kanaaa224@outlook.com>
2025-06-10 11:47:13 +0900

    [ update ] ボス1 移動速度 4分の3

--- source/Characters/Vaillant.hpp
+++ source/Characters/Vaillant.hpp
@@ -5,7 +5,7 @@
 
 # define VAILLANT_SIZE { 203, 233 }
 # define VAILLANT_MAX_HP 100
-# define VAILLANT_WALK_POWER 10
+# define VAILLANT_WALK_POWER 7.5
 # define VAILLANT_JUMP_POWER 2500
 
 enum class VaillantState {
