51f60a963303ddef912aaa49f63373fb19e722a0
moyakino <104491816+moyakino@users.noreply.github.com>
2025-06-11 10:06:45 +0900

    ボスと壁が再生成されるように修正

--- source/Stages/1.cpp
+++ source/Stages/1.cpp
@@ -46,6 +46,8 @@ void Stage1::initialize()
 	camera = Camera2D(Vec2{ (Scene::Width() / 2), (Scene::Height() / 2) }, 1.0, CameraControl::None_);
 
 	BossEreaflg = false;
+	excuted = false;
+	boss_spawn = false;
 }
 
 void Stage1::update()
@@ -137,14 +139,12 @@ void Stage1::update()
 			}
 		}
 
-		static bool excuted;
 		if (!excuted && BossEreaflg)
 		{
 			createObject<Wall>(Vec2(STAGE1_WIDTH, 500.0));
 			excuted = true;
 		}
 
-		static bool boss_spawn;
 		if (!boss_spawn && BossEreaflg)
 		{
 			createObject<Vaillant>(Vec2{ (Scene::Width() / 2) + 4900, 500 });
