5f926d6ee26e84c39a376420baeaf2e43680d5b3
kanaaa224 <kanaaa224@outlook.com>
2025-06-11 16:36:19 +0900

    [ update ] ダメージ量 調整

--- source/Objects/HitBox.cpp
+++ source/Objects/HitBox.cpp
@@ -54,7 +54,7 @@ void HitBox::onHit(ObjectBase& object)
 
 	if (Vaillant* vaillant = dynamic_cast<Vaillant*>(&object))
 	{
-		vaillant->applyDamage(20);
+		vaillant->applyDamage(5);
 
 		destroy();
 	}
