143136278f8349f02712adcb4731fa931b7b8fe4
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-06-11 10:12:33 +0900

    [fix] 空を飛ぶ敵の攻撃を遅めに修正

--- source/Characters/Enemies/Flot/Flot.hpp
+++ source/Characters/Enemies/Flot/Flot.hpp
@@ -5,7 +5,7 @@
 //画像切替速度
 #define Flot_IMG_CT_IDLE           0.07
 #define Flot_IMG_CT_ATTACK_POSTION 0.1
-#define Flot_IMG_CT_ATTACK         0.1
+#define Flot_IMG_CT_ATTACK         0.2
 #define Flot_IMG_CT_GET_ATTACK     0.05
 #define Flot_IMG_CT_DIE            0.2
 
