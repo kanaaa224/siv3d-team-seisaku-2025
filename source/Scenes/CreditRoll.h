22b04097262abb7a799cc65528da3e9f774c17b6
REI209 <138412628+REI209@users.noreply.github.com>
2025-06-11 13:57:27 +0900

    credit

---
+++ source/Scenes/CreditRoll.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+# include "../Common.hpp"
+
+class CreditRoll : public App::Scene
+{
+public:
+	CreditRoll(const InitData& init);
+
+	void update() override;
+	void draw() const override;
+
+private:
+	RoundRect m_exitButton{ Arg::center(640, 480), 300, 60, 8 };
+
+	Transition m_exitTransition{ 0.2s, 0.2s };
+
+	// 0: PLAY (スタート), 1: EXIT (終了)
+	int m_selectedButtonIndex = 0;
+
+	Texture credit_background;
+
+	double m_scrollY;
+};
+
