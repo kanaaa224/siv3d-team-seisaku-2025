83ed89c05c9a924a2c3fe1ea1db9d2ccb43b6314
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-06-17 14:31:54 +0900

    [add] エフェクトアセットの追加

--- source/main.cpp
+++ source/main.cpp
@@ -88,6 +88,7 @@ void AssetsRegistration()
 		TextureAsset::Register({ U"Effect 1 14",  { U"Effect" } }, U"../assets/images/effect/1/14.png");
 		TextureAsset::Register({ U"Effect 1 27",  { U"Effect" } }, U"../assets/images/effect/1/27.png");
 		TextureAsset::Register({ U"Excalimation Mark",  { U"Effect" } }, U"../assets/images/effect/excalimation mark2.png");
+		TextureAsset::Register({ U"SmallHit",  { U"Effect" } }, U"../assets/images/effect/Hit/Effect_SmallHit_1_532x528.png");
 	}
 
 	//GameUI
