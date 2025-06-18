965cad780346034eccffd96cd40934a5b20a9226
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-06-18 13:54:35 +0900

    [update] ヒットエフェクトクラスの関数と変数の作成

--- source/Characters/Enemies/Effect/GetDamageEffect.cpp
+++ source/Characters/Enemies/Effect/GetDamageEffect.cpp
@@ -1,1 +1,17 @@
 ﻿#include "GetDamageEffect.h"
+
+GetDamageEffect::GetDamageEffect(P2World& world, const Vec2& postion, ObjectBase& ownerClass)
+{
+}
+
+GetDamageEffect::~GetDamageEffect()
+{
+}
+
+void GetDamageEffect::update()
+{
+}
+
+void GetDamageEffect::draw() const
+{
+}
--- source/Characters/Enemies/EnemyBase.cpp
+++ source/Characters/Enemies/EnemyBase.cpp
@@ -306,11 +306,11 @@ void EnemyBase::getDamage(float damage)
 
 		//ダメージを受けた時のノックバック処理
 		if (playerPos.x > body.getPos().x) {
-			body.applyLinearImpulse(Vec2{ -130,0 });
+			body.applyLinearImpulse(Vec2{ -13,0 });
 			img_flipFlg = false;
 		}
 		else {
-			body.applyLinearImpulse(Vec2{ 130,0 });
+			body.applyLinearImpulse(Vec2{ 13,0 });
 			img_flipFlg = true;
 		}
 	}
