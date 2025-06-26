99de393d7523bf2f050bca727b3fb7c34dc09d47
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-06-26 17:12:05 +0900

    [update] StarClass

---
+++ source/Effects/Star.cpp
@@ -0,0 +1,19 @@
+﻿#include "Star.h"
+
+Star::Star(Vec2 pos)
+{
+}
+
+Star::~Star()
+{
+}
+
+void Star::update()
+{
+}
+
+void Star::draw() const
+{
+	//画像の描画
+	TextureAsset(U"")(margin, IMG_SIZE, IMG_SIZE).resized(reSize).drawAt(position);
+}
---
+++ source/Effects/Star.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+class Star
+{
+#define IMG_SIZE 32
+
+public:
+	Star(Vec2 pos);
+	~Star();
+
+	void update();
+	void draw() const;
+
+private:
+	Vec2 position;
+	Vec2 margin;
+	double reSize;
+};
+
