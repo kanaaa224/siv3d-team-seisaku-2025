015e9fb639d34ae6cf1634ac4d8fc6bbb14e48cb
kanaaa224 <kanaaa224@outlook.com>
2025-06-26 22:47:29 +0900

    [ update ] ボスの触手を実装中

--- source/Characters/Tentacles.hpp
+++ source/Characters/Tentacles.hpp
@@ -1,1 +1,35 @@
-﻿
+﻿# pragma once
+
+# include "Base.hpp"
+
+# define TENTACLES_SIZE { 150, 150 }
+# define TENTACLES_MAX_HP 100
+
+class Tentacles : public CharacterBase
+{
+public:
+	Tentacles(P2World& world, const Vec2& position = { 0, 0 });
+	~Tentacles() = default;
+
+	void update() override;
+	void draw() const override;
+
+	void destroy() override;
+	void die() override;
+
+	void onHit(ObjectBase& object) override;
+	void onDamaged(float amount) override;
+	
+	void setPlayerPosition(Vec2 pos = { 0, 0 }) { player_position = pos; }
+
+private:
+	Vec2 position;
+	Vec2 start_position;
+	Vec2 player_position;
+	
+	SizeF size;
+	
+	bool damaged;
+	bool die_executed;
+	bool destroy_executed;
+};
