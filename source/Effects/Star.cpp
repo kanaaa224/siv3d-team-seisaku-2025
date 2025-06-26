a026042c61bf5d1768eb4289ceccaad6d137d50e
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-06-26 17:25:13 +0900

    [update] starのアニメーション

--- source/Effects/Star.cpp
+++ source/Effects/Star.cpp
@@ -1,7 +1,11 @@
 ﻿#include "Star.h"
 
-Star::Star(Vec2 pos)
+Star::Star(P2World& world, const Vec2& pos)
+	: ObjectBase(world, pos)
 {
+	position = pos;
+	reSize = Random(IMG_SIZE, IMG_SIZE + 20);
+	margin = Vec2{ 0,0 };
 }
 
 Star::~Star()
@@ -10,10 +14,20 @@ Star::~Star()
 
 void Star::update()
 {
+	time_ct += Scene::DeltaTime();
+
+	if (time_ct >= CHANGE_IMG) {
+		margin.x += IMG_SIZE;
+		if (margin.x >= IMG_SIZE * IMG_NUM) {
+			deleteSelf();
+		}
+
+		time_ct = 0;
+	}
 }
 
 void Star::draw() const
 {
 	//画像の描画
-	TextureAsset(U"")(margin, IMG_SIZE, IMG_SIZE).resized(reSize).drawAt(position);
+	TextureAsset(U"Star")(margin, IMG_SIZE, IMG_SIZE).resized(reSize).drawAt(position);
 }
--- source/Effects/Star.h
+++ source/Effects/Star.h
@@ -1,16 +1,23 @@
 ﻿#pragma once
-class Star
+
+#include "../Objects/Base.hpp"
+
+class Star : public ObjectBase
 {
+#define IMG_NUM 4
 #define IMG_SIZE 32
+#define CHANGE_IMG 0.2
 
 public:
-	Star(Vec2 pos);
+	Star(P2World& world, const Vec2& pos);
 	~Star();
 
 	void update();
 	void draw() const;
 
 private:
+	double time_ct;
+
 	Vec2 position;
 	Vec2 margin;
 	double reSize;
